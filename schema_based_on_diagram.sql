eb69ebd8784947dca4372116d9eeac27cdb28bb0
iLynette <lynetteacholah@icloud.com>
2022-05-10 19:00:59 +0300

    Create indexes for the foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -49,3 +49,13 @@ CREATE TABLE invoice_items (
     treatment_id INT REFERENCES treatments(id),
     PRIMARY KEY(id)
 );
+
+-- create indexes for FK
+CREATE INDEX patient_id_asc ON medical_histories(patient_id ASC);
+CREATE INDEX medical_history_id_asc ON medical_treatments(medical_history_id ASC);
+CREATE INDEX treatment_id_asc ON medical_treatments(treatment_id ASC);
+CREATE INDEX medical_history_id_asc ON invoices(medical_history_id ASC);
+CREATE INDEX treatment_id_asc ON invoice_items(treatment_id ASC);
+CREATE INDEX invoice_id_asc ON invoice_items(invoice_id ASC);
+
+
